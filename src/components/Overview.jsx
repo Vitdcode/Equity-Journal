b77c7a8fc6bbf8bb8b7b3b147f76ac33227ab266
Vitdcode <vitalijdietrich@gmail.com>
2025-02-22 22:46:51 +0100

    feat: Refactored code in Overview component by splitting it into several components to have cleaner and more readable code.

--- src/components/Overview.jsx
+++ src/components/Overview.jsx
@@ -1,11 +1,9 @@
-import { useState } from "react";
 import { useOutletContext } from "react-router-dom";
 import { FaArrowTrendUp } from "react-icons/fa6";
 import { FaArrowTrendDown } from "react-icons/fa6";
 
 export default function Overview() {
   const { data } = useOutletContext();
-  const [returnPositive, setReturnPostitive] = useState(true);
 
   const totalAmount = Object.values(data[data.length - 1])
     .flatMap((investments) => Object.values(investments))
@@ -17,7 +15,6 @@ export default function Overview() {
 
   const returnOnInvestment = (index) => {
     if (data[index - 1] === undefined) return;
-    console.log(data[index - 1]);
 
     function amountHelperFunction(index) {
       return Object.values(data[index])
@@ -26,12 +23,9 @@ export default function Overview() {
     }
 
     const currentAmount = amountHelperFunction(index);
-    const lastAmount = amountHelperFunction(index - 1);
+    const previousAmount = amountHelperFunction(index - 1);
 
-    const amount = `${(currentAmount - lastAmount).toFixed(2)}`;
-    /*     amount > 0 ? setReturnPostitive(true) : setReturnPostitive(false); */
-
-    return amount;
+    return (currentAmount - previousAmount).toFixed(2);
   };
 
   return (
@@ -39,46 +33,55 @@ export default function Overview() {
       <span className="text-xl font-bold text-green-600 [text-shadow:1px_1px_1px_rgba(0,0,0,0.2)]">
         Total amount: ${totalAmount.toFixed(2)}
       </span>
-      {data.map((entry, index) => {
-        const amount = returnOnInvestment(index);
-
-        return (
-          <div key={index}>
-            {amount !== undefined && (
-              <>
-                {amount > 0 ? (
-                  <div className="flex items-center justify-end mr-2 mb-2 gap-2 ">
-                    <FaArrowTrendUp />
-                    <span className="text-green-600">{amount}</span>
-                  </div>
-                ) : (
-                  <div>
-                    <FaArrowTrendDown />
-                    <span className="text-red-600">{amount}</span>
-                  </div>
-                )}
-              </>
-            )}
-
-            {Object.entries(entry).map(([date, investments]) => (
-              <div
-                key={date}
-                className="flex flex-col items-center gap-10 shadow bg-white p-5 rounded-xl"
-              >
-                <span className="text-xl font-bold">{date}</span>
-                <div className="flex gap-20 bg-base-200 border border-base-300 rounded-xl p-2">
-                  {Object.entries(investments).map(([investmentType, amount]) => (
-                    <span key={investmentType}>
-                      {investmentType}: ${amount.toFixed(2)}
-                    </span>
-                  ))}
-                  <span>Total amount: {totalAmountPerQuartal(investments)}</span>
-                </div>
-              </div>
-            ))}
-          </div>
-        );
-      })}
+      {data.map((entry, index) => (
+        <div key={index}>
+          <InvestmentChange amount={returnOnInvestment(index)} />
+          <InvestmentData entry={entry} totalAmountPerQuartal={totalAmountPerQuartal} />
+        </div>
+      ))}
     </div>
   );
 }
+
+const InvestmentData = ({ entry, totalAmountPerQuartal }) => {
+  return (
+    <>
+      {Object.entries(entry).map(([date, investments]) => (
+        <div
+          key={date}
+          className="flex flex-col items-center gap-10 shadow bg-white p-5 rounded-xl"
+        >
+          <span className="text-xl font-bold">{date}</span>
+          <div className="flex gap-20 bg-base-200 border border-base-300 rounded-xl p-2">
+            {Object.entries(investments).map(([investmentType, amount]) => (
+              <span key={investmentType}>
+                {investmentType}: ${amount.toFixed(2)}
+              </span>
+            ))}
+            <span>Total amount: {totalAmountPerQuartal(investments)}</span>
+          </div>
+        </div>
+      ))}
+    </>
+  );
+};
+
+const InvestmentChange = ({ amount }) => {
+  if (amount === undefined) return;
+
+  return (
+    <div>
+      {amount > 0 ? (
+        <div className="flex items-center justify-end mr-2 mb-2 gap-2 ">
+          <FaArrowTrendUp />
+          <span className="text-green-600">${amount}</span>
+        </div>
+      ) : (
+        <div>
+          <FaArrowTrendDown />
+          <span className="text-red-600">${amount}</span>
+        </div>
+      )}
+    </div>
+  );
+};
