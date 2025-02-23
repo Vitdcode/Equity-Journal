7c2ce45778cb1e47b8ab74608c7776becb800f79
Vitdcode <vitalijdietrich@gmail.com>
2025-02-23 01:09:39 +0100

    feat: Added App logo and text

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { Link, Outlet } from "react-router-dom";
 import { MdOutlineSpaceDashboard } from "react-icons/md";
 import { MdCalendarMonth } from "react-icons/md";
 import { LuPaintbrushVertical } from "react-icons/lu";
+import logo from "./images/logo.png";
+import logoText from "./images/logo-text.png";
+import { GiCash } from "react-icons/gi";
 
 function App() {
   const [theme, setTheme] = useState("nord");
+  /*  const [investmentReturns, setInvestmentreturns] = useState([]); */
+  const investmentReturnsRef = useRef([]);
   const [data, setData] = useState([
     {
       "29.07.2024": {
@@ -23,23 +28,44 @@ function App() {
         Cash: 3000,
       },
     },
+    {
+      "01.02.2025": {
+        Invested: 29276,
+        "Cash Trade Republic": 3994,
+        "C24 Tagesgeld": 1132,
+        Cash: 3000,
+      },
+    },
   ]);
 
   return (
     <div className="main-wrapper">
-      <div className=" h-screen flex flex-col p-25 gap-10 bg-base-200 border border-base-300 rounded-box ">
-        <Link to="overview" className=" justify-start  btn btn-accent btn-xl">
-          <MdOutlineSpaceDashboard /> Overview
-        </Link>
-        <Link to={"add-month"} className="btn btn-accent justify-start btn-xl">
-          <MdCalendarMonth /> Add month
-        </Link>
-        <Link to={"change-theme"} className="btn btn-accent justify-start btn-xl">
-          <LuPaintbrushVertical /> Change App Theme
-        </Link>
+      <div className=" h-screen flex flex-col  gap-10 bg-base-200 border border-base-300 rounded-box ">
+        <div className="relative">
+          <img
+            src={logoText}
+            alt="Logo Text"
+            className="bg-base-100 border-2 border-base-300 rounded-xl "
+          />
+          <GiCash fontSize={"3rem"} className="rotate-16 absolute bottom-0 right-0" />
+
+          {/*  <img src={logo} alt="Logo image of the App" className="h-20 w-20" /> */}
+        </div>
+
+        <div className="flex flex-col p-10 gap-10">
+          <Link to="overview" className=" justify-start  btn btn-accent btn-xl">
+            <MdOutlineSpaceDashboard /> Overview
+          </Link>
+          <Link to={"add-month"} className="btn btn-accent justify-start btn-xl">
+            <MdCalendarMonth /> Add month
+          </Link>
+          <Link to={"change-theme"} className="btn btn-accent justify-start btn-xl">
+            <LuPaintbrushVertical /> Change App Theme
+          </Link>
+        </div>
       </div>
       <div className="flex flex-col items-center gap-20 mt-10">
-        <Outlet context={{ theme, setTheme, data }} />
+        <Outlet context={{ theme, setTheme, data, investmentReturnsRef }} />
       </div>
     </div>
   );
--- src/components/Overview.jsx
+++ src/components/Overview.jsx
@@ -1,9 +1,14 @@
 import { useOutletContext } from "react-router-dom";
 import { FaArrowTrendUp } from "react-icons/fa6";
 import { FaArrowTrendDown } from "react-icons/fa6";
+import { FaWallet } from "react-icons/fa";
+import { SiCashapp } from "react-icons/si";
+import { FaRegCalendarAlt } from "react-icons/fa";
+import { TbBrandCashapp } from "react-icons/tb";
+import PropTypes from "prop-types";
 
 export default function Overview() {
-  const { data } = useOutletContext();
+  const { data, investmentReturnsRef } = useOutletContext();
 
   const totalAmount = Object.values(data[data.length - 1])
     .flatMap((investments) => Object.values(investments))
@@ -11,6 +16,10 @@ export default function Overview() {
 
   const totalAmountPerQuartal = (investments) => {
     return Object.values(investments).reduce((sum, num) => (sum += num), 0);
+  };
+
+  const totalChange = () => {
+    return investmentReturnsRef.current.reduce((sum, num) => (sum += num), 0);
   };
 
   const returnOnInvestment = (index) => {
@@ -24,41 +33,55 @@ export default function Overview() {
 
     const currentAmount = amountHelperFunction(index);
     const previousAmount = amountHelperFunction(index - 1);
-
-    return (currentAmount - previousAmount).toFixed(2);
+    investmentReturnsRef.current.push(currentAmount - previousAmount);
+    return [currentAmount, previousAmount];
   };
 
   return (
     <div className="flex flex-col gap-10">
-      <span className="text-xl font-bold text-green-600 [text-shadow:1px_1px_1px_rgba(0,0,0,0.2)]">
-        Total amount: ${totalAmount.toFixed(2)}
+      <span className="flex items-center gap-5 stat-value text-emerald-400 bg-base-200 border border-base-300 rounded-xl p-2 w-fit">
+        <FaWallet /> Total amount: ${totalAmount.toFixed(2)}
+      </span>
+      <span className="flex flex-col gap-3 text-emerald-400 bg-base-200 border border-base-300 rounded-xl p-2 w-fit">
+        <FaArrowTrendUp /> Started: 12.02.2024 <br></br> Growth: ${totalChange().toFixed(2)} in{" "}
+        {(data.length - 1) * 3} months ~ {((data.length - 1) * 3) / 12} years
       </span>
       {data.map((entry, index) => (
         <div key={index}>
           <InvestmentChange amount={returnOnInvestment(index)} />
-          <InvestmentData entry={entry} totalAmountPerQuartal={totalAmountPerQuartal} />
+          <InvestmentData
+            entry={entry}
+            totalAmountPerQuartal={totalAmountPerQuartal}
+            investmentReturnsRef={investmentReturnsRef}
+          />
         </div>
       ))}
     </div>
   );
 }
 
-const InvestmentData = ({ entry, totalAmountPerQuartal }) => {
+const InvestmentData = ({ entry, totalAmountPerQuartal, investmentReturnsRef }) => {
+  investmentReturnsRef.current.length = 0; //resetting the ref so it doesn't keep pushing the same numbers
   return (
     <>
       {Object.entries(entry).map(([date, investments]) => (
         <div
           key={date}
-          className="flex flex-col items-center gap-10 shadow bg-white p-5 rounded-xl"
+          className="flex flex-col items-center gap-10 bg-base-200 border border-base-300 rounded-xl p-2"
         >
-          <span className="text-xl font-bold">{date}</span>
-          <div className="flex gap-20 bg-base-200 border border-base-300 rounded-xl p-2">
+          <span className="flex items-center gap-2 text-xl font-bold ">
+            <FaRegCalendarAlt /> {date}
+          </span>
+          <div className="flex gap-20 bg-base-100 border border-base-100 rounded-xl p-2 shadow">
             {Object.entries(investments).map(([investmentType, amount]) => (
-              <span key={investmentType}>
-                {investmentType}: ${amount.toFixed(2)}
+              <span key={investmentType} className="flex items-center">
+                <TbBrandCashapp /> {investmentType}: ${amount.toFixed(2)}
               </span>
             ))}
-            <span>Total amount: {totalAmountPerQuartal(investments)}</span>
+            <span className="flex items-center gap-2">
+              <SiCashapp />
+              Total amount: {totalAmountPerQuartal(investments)}
+            </span>
           </div>
         </div>
       ))}
@@ -66,22 +89,39 @@ const InvestmentData = ({ entry, totalAmountPerQuartal }) => {
   );
 };
 
+InvestmentData.propTypes = {
+  entry: PropTypes.object,
+  totalAmountPerQuartal: PropTypes.func,
+  investmentReturnsRef: PropTypes.array,
+};
+
 const InvestmentChange = ({ amount }) => {
   if (amount === undefined) return;
 
+  const currentAmount = amount[0];
+  const previousAmount = amount[1];
+
+  const change = (currentAmount - previousAmount).toFixed(2);
+  const changePercantage = (((currentAmount - previousAmount) / currentAmount) * 100).toFixed(2);
+
   return (
     <div>
-      {amount > 0 ? (
+      {change > 0 ? (
         <div className="flex items-center justify-end mr-2 mb-2 gap-2 ">
+          <span className="text-green-600">${change}</span>
           <FaArrowTrendUp />
-          <span className="text-green-600">${amount}</span>
+          <span className="text-green-600">{changePercantage}%</span>
         </div>
       ) : (
         <div>
           <FaArrowTrendDown />
-          <span className="text-red-600">${amount}</span>
+          <span className="text-red-600">${change}</span>
         </div>
       )}
     </div>
   );
+};
+
+InvestmentChange.propTypes = {
+  amount: PropTypes.array,
 };
